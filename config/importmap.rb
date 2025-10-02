1c94a013cd0cf3ae99e3e0b80f8842ba7bff411d
James Oneyibo <esiribiz@gmail.com>
2025-10-02 12:02:32 +0300

    Fix: Move JavaScript files to proper locations for modern asset pipeline
    - Moved admin_notifications.js and admin.js from app/assets/javascripts/ to app/javascript/
    - Updated importmap.rb to include admin_notifications and admin
    - Removed empty app/assets/javascripts directory
    - This fixes the Propshaft::MissingAssetError for admin_notifications.js
    - Admin panel should now load without JavaScript asset errors

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,8 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
+pin "admin_notifications"
+pin "admin"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
